89e17f5fafded963cc4f2c1601d1df2f325b716d
Ivan <ivan.venelinov.shulev@gmail.com>
2016-02-16 15:07:35 +0700

    if it is units singular, restrict by membership

--- page.php
+++ page.php
@@ -1,4 +1,17 @@
+<?php global $current_user; ?>
 <?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/page', 'header'); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
+	<?php get_template_part('templates/page', 'header'); ?>
+	<?php if(is_singular( 'unit' )) : ?>
+		<?php if(pmpro_hasMembershipLevel('1') ) : ?>
+			<?php get_template_part('templates/content', 'page'); ?>
+		<?php else : ?>
+			<?php global $pmpro_pages; ?>
+			<div class="jumbotron">
+				<h2>This area is for paying members only.</h2>
+				<p>Follow <a href="<?php echo get_page_link( $post = $pmpro_pages['levels'], $leavename, $sample ); ?>">this link</a> to set up your account.</p>
+			</div>
+		<?php endif; ?>
+	<?php else : ?>
+		<?php get_template_part('templates/content', 'page'); ?>
+	<?php endif; ?>
 <?php endwhile; ?>
