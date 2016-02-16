04f5dd328f14ff422d798f444dd129bba650b7b6
Ivan <ivan.venelinov.shulev@gmail.com>
2016-02-16 15:06:11 +0700

    display register button, instead of course signup, if user is not logged in

--- front-page.php
+++ front-page.php
@@ -64,7 +64,7 @@ if ( $the_query->have_posts() ) : ?>
 				echo '<a href="' . esc_url(get_permalink()) . '"><img src="' . get_post_meta(get_the_ID(), 'featured_url', true) . '" class="img-responsive"></a>';
 				echo '<span class="course-category label label-'. $category_display_label .'">' . $category_object->name . '</span>';
 				echo '<a href="' . esc_url(get_permalink()) . '">' . get_the_title() . '</a>';
-				echo '<p>' . trim(do_shortcode( '[course_end label="" label_tag="" course_id="' . get_the_ID() . '"]') ) . ' / ' . trim(do_shortcode( '[course_time_estimation course_id="' . get_the_ID() . '"]' ) ) . '</p>';
+				echo '<p>' . get_the_date() . ' / ' . trim(do_shortcode( '[course_time_estimation course_id="' . get_the_ID() . '"]' ) ) . '</p>';
 				echo '</div>';
 				echo '</div>';
 			}
