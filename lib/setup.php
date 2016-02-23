345f7d96594d1f5aa448ccf2a5c5e3388b36f15b
Ivan <ivan.venelinov.shulev@gmail.com>
2016-02-23 20:37:44 +0700

    changed footer widget title elements from h3 to h6

--- lib/setup.php
+++ lib/setup.php
@@ -66,10 +66,10 @@ function widgets_init() {
   register_sidebar(array(
     'name'          => __('Footer', 'sage'),
     'id'            => 'sidebar-footer',
-    'before_widget' => '<section class="widget %1$s %2$s">',
-    'after_widget'  => '</section>',
-    'before_title'  => '<h3>',
-    'after_title'   => '</h3>'
+    'before_widget' => '<div class="col-md-3">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h6>',
+    'after_title'   => '</h6>'
   ));
 }
 add_action('widgets_init', __NAMESPACE__ . '\\widgets_init');
