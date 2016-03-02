6349470822fee9ae342ada561ef81e9634fdb6f1
Ivan <ivan.venelinov.shulev@gmail.com>
2016-03-02 14:14:55 +0700

    finished adding front page widgets

--- lib/widgets.php
+++ lib/widgets.php
@@ -208,7 +208,7 @@ class Front_Top_CTA_Widget extends WP_Widget {
 		// The Query
 		$ctas = get_posts( $query_args );
 		echo $args['before_widget'];
-		echo '<div class="row">';
+		echo '<div class="row front-widget">';
 			foreach ( $ctas as $cta ) : setup_postdata( $cta ); ?>
 				<?php $meta = get_post_meta($cta->ID); ?>
 				<div class="col-md-4 text-center">
@@ -298,7 +298,7 @@ class Signup_CTA_Widget extends WP_Widget {
 		$big_title = $instance['big-title'];
 		// The Query
 		echo $args['before_widget'];
-		echo '<div class="row text-center">';
+		echo '<div class="row text-center front-widget">';
 			if($big_title) {
 				echo '<h2>' . $post['post_title'] . '</h2>';
 			} else {
@@ -389,7 +389,7 @@ class Display_Latest_Courses_Widget extends WP_Widget {
 			echo '</div>';
 
 			echo $args['before_widget'];
-			echo '<div class="full-width-color">';
+			echo '<div class="full-width-color front-widget">';
 				echo '<div class="container">';
 					echo '<h3>' . $title . '</h3>';
 					echo '<div class="row courses">';
@@ -450,6 +450,169 @@ class Display_Latest_Courses_Widget extends WP_Widget {
 
 } // class Display_Latest_Courses_Widget
 
+class Display_Testimonials_Widget extends WP_Widget {
+
+	/**
+	 * Register widget with WordPress.
+	 */
+	function __construct() {
+		parent::__construct(
+			'testimonials_widget', // Base ID
+			__( 'Display testimonials', 'da' ), // Name
+			array( 'description' => __( 'Display testimonials for the front page', 'da' ),
+				'title' => 'Display testimonials',
+			) // Args
+		);
+	}
+
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+		$testimonials = get_posts( $args = array('post_type' => 'testimonial', 'posts_per_page' => 3, 'post_status' => 'publish') );
+		$title = $instance['title'];
+		if(!empty($testimonials)) {
+
+			echo $args['before_widget'];
+			echo '<div class="row front-widget">';
+			echo '<h3 class="text-center">' . $title . '</h3>';
+			foreach ( $testimonials as $testimonial ) : setup_postdata( $testimonial ); ?>
+				<?php $author_link = get_post_meta( $testimonial->ID, $key = 'author_url', $single = true ); ?>
+				<div class="col-md-4 text-center">
+					<?php if(has_post_thumbnail( $post_id = $testimonial->ID ) ) : ?>
+						<?php echo get_the_post_thumbnail( $post = $testimonial->ID, $size = 'thumbnail', $attr = array('class' => 'img-circle') ); ?>
+					<?php endif; ?>
+					<h4><?php echo $testimonial->post_title; ?></h4>
+					<?php if ( '' !== $author_link) : ?>
+						<p><?php echo $author_link; ?></p>
+					<?php endif ?>
+					<p><em><?php echo $testimonial->post_excerpt; ?></em></p>
+				</div>
+			<?php endforeach;
+			echo '</div>';
+			echo $args['after_widget'];
+		}
+		wp_reset_postdata();
+	}
+
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form( $instance ) {
+		$title = ! empty( $instance['title'] ) ? $instance['title'] : __( 'What people are saying', 'da' );
+		?>
+		<p><label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+	<?php }
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance[ 'title' ] = $new_instance[ 'title' ];
+		return $instance;
+	}
+
+} // class Display_Testimonials_Widget
+
+class Display_Partners_Widget extends WP_Widget {
+
+	/**
+	 * Register widget with WordPress.
+	 */
+	function __construct() {
+		parent::__construct(
+			'partners_widget', // Base ID
+			__( 'Display partners', 'da' ), // Name
+			array( 'description' => __( 'Display partners for the front page', 'da' ),
+				'title' => 'Display partners',
+			) // Args
+		);
+	}
+
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+		$partners = get_posts( $args = array('post_type' => 'partner', 'posts_per_page' => -1, 'post_status' => 'publish') );
+		$title = $instance['title'];
+		if(!empty($partners)) {
+
+			echo $args['before_widget'];
+			echo '<div class="row text-center front-widget">';
+				echo '<h3>' . $title . '</h3>';
+				foreach ( $partners as $partner ) : setup_postdata( $partner ); ?>
+					<?php $partner_url = get_post_meta( $partner->ID, $key = 'partner_url', $single = true ); ?>
+					<?php if (has_post_thumbnail( $post_id = $partner->ID )): ?>
+						<a class="partner-logo" href="<?php echo esc_url( $url = $partner_url, $protocols, $_context ); ?>">
+							<?php echo get_the_post_thumbnail( $post_id = $partner->ID, $size = 'medium', $attr ); ?>
+						</a>
+					<?php endif; ?>
+				<?php endforeach;
+			echo '</div>';
+			echo $args['after_widget'];
+		}
+		wp_reset_postdata();
+	}
+
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form( $instance ) {
+		$title = ! empty( $instance['title'] ) ? $instance['title'] : __( 'Exclusive partner deals to kickstart your ventures', 'da' );
+		?>
+		<p><label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+	<?php }
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance[ 'title' ] = $new_instance[ 'title' ];
+		return $instance;
+	}
+
+} // class Display_Partners_Widget
+
 // register custom widgets
 function register_custom_widgets() {
 	register_widget( 'Stats_Widget' );
@@ -457,5 +620,7 @@ function register_custom_widgets() {
 	register_widget( 'Front_Top_CTA_Widget' );
 	register_widget( 'Signup_CTA_Widget' );
 	register_widget( 'Display_Latest_Courses_Widget' );
+	register_widget( 'Display_Testimonials_Widget' );
+	register_widget( 'Display_Partners_Widget' );
 }
 add_action( 'widgets_init', 'register_custom_widgets' );
