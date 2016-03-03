48b93fe9fe7290ad331576db76898f9aa1d8283c
Ivan <ivan.venelinov.shulev@gmail.com>
2016-03-03 14:15:55 +0700

    fixed a bug that was caused by the misplacement of the alert

--- templates/profile-view.php
+++ templates/profile-view.php
@@ -47,29 +47,25 @@
 ?>
 <?php while (have_posts()) : the_post(); ?>
 	<?php if((int)$user_id === get_current_user_id() && 'active' !== $payment_status) { ?>
-		<div class="container">
-			<div class="alert alert-warning alert-dismissible fade in" role="alert">
-				<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
-				<p>Your profile is still not visible to others. Please select a <a href="<?php echo get_page_link( $post = $pmpro_pages['levels'], $leavename, $sample ); ?>">payment plan</a>.</p>
-			</div>
+		<div class="alert alert-warning alert-dismissible fade in" role="alert">
+			<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
+			<p>Your profile is still not visible to others. Please select a <a href="<?php echo esc_url( $url = home_url( $path = '/pricing', $scheme = 'relative' ), $protocols, $_context ); ?>">payment plan</a>.</p>
 		</div>
 	<?php } ?>
-	<div class="container">
-		<div class="row profile-view">
-			<div class="col-md-6 col-md-offset-3 text-center">
-				<?php if( !is_page( 'settings' ) && (int)$user_id === get_current_user_id()) : ?>
-					<p class="edit-button"><a href="<?php echo home_url( 'settings', 'relative' ); ?>">Edit profile</a></p>
-				<?php endif; ?>
-				<?php echo get_avatar( $id_or_email = $user_id, $size, $default, $alt, $args = array( 'class' => 'img-circle' )); ?>
-				<h1><?php echo $fname . ' ' . $lname; ?></h1>
-				<div class="row"><?php echo ('1' === $occupation_engineer ? '<a href="' . home_url( 'members/?occupation=engineer', 'relative' ) . '"><span class="label label-success">Engineer</span></a>' : ''); ?><?php echo ('1' === $occupation_designer ? '<a href="' . home_url( 'members/?occupation=designer', 'relative' ) . '"><span class="label label-danger">Designer</span></a>' : ''); ?><?php echo ('1' === $occupation_entrepreneur ? '<a href="' . home_url( 'members/?occupation=entrepreneur', 'relative' ) . '"><span class="label label-primary">Entrepreneur</span></a>' : ''); ?></div>
-				<p class="text">Member since <?php echo date('F, Y', strtotime($student->user_registered)); ?> <?php if( '' !== $user_city && '' !== $user_country) : ?>&#8226; Location: <a href="<?php echo home_url( 'members/?country=' . $user_country, 'relative' ); ?> "><?php echo $user_city . ', ' . $user_country; ?></a><?php endif; ?></p>
-				<p class="text description"><?php echo $user_description; ?></p>
-				<div class="row social">
-					<?php if('' !== $smediaurl_facebook) : ?><a href="<?php echo $smediaurl_facebook; ?>"><span class="fa fa-facebook"></span></a><?php endif; ?>
-					<?php if('' !== $smediaurl_twitter) : ?><a href="<?php echo $smediaurl_twitter; ?>"><span class="fa fa-twitter"></span></a><?php endif; ?>
-					<?php if('' !== $smediaurl_googleplus) : ?><a href="<?php echo $smediaurl_googleplus; ?>"><span class="fa fa-google-plus"></span></a><?php endif; ?>
-				</div>
+	<div class="row profile-view">
+		<div class="col-md-6 col-md-offset-3 text-center">
+			<?php if( !is_page( 'settings' ) && (int)$user_id === get_current_user_id()) : ?>
+				<p class="edit-button"><a href="<?php echo home_url( 'settings', 'relative' ); ?>">Edit profile</a></p>
+			<?php endif; ?>
+			<?php echo get_avatar( $id_or_email = $user_id, $size, $default, $alt, $args = array( 'class' => 'img-circle' )); ?>
+			<h1><?php echo $fname . ' ' . $lname; ?></h1>
+			<div class="row"><?php echo ('1' === $occupation_engineer ? '<a href="' . home_url( 'members/?occupation=engineer', 'relative' ) . '"><span class="label label-success">Engineer</span></a>' : ''); ?><?php echo ('1' === $occupation_designer ? '<a href="' . home_url( 'members/?occupation=designer', 'relative' ) . '"><span class="label label-danger">Designer</span></a>' : ''); ?><?php echo ('1' === $occupation_entrepreneur ? '<a href="' . home_url( 'members/?occupation=entrepreneur', 'relative' ) . '"><span class="label label-primary">Entrepreneur</span></a>' : ''); ?></div>
+			<p class="text">Member since <?php echo date('F, Y', strtotime($student->user_registered)); ?> <?php if( '' !== $user_city && '' !== $user_country) : ?>&#8226; Location: <a href="<?php echo home_url( 'members/?country=' . $user_country, 'relative' ); ?> "><?php echo $user_city . ', ' . $user_country; ?></a><?php endif; ?></p>
+			<p class="text description"><?php echo $user_description; ?></p>
+			<div class="row social">
+				<?php if('' !== $smediaurl_facebook) : ?><a href="<?php echo $smediaurl_facebook; ?>"><span class="fa fa-facebook"></span></a><?php endif; ?>
+				<?php if('' !== $smediaurl_twitter) : ?><a href="<?php echo $smediaurl_twitter; ?>"><span class="fa fa-twitter"></span></a><?php endif; ?>
+				<?php if('' !== $smediaurl_googleplus) : ?><a href="<?php echo $smediaurl_googleplus; ?>"><span class="fa fa-google-plus"></span></a><?php endif; ?>
 			</div>
 		</div>
 	</div>
